e353d7754889638b45ff2fc72541e7d8b18987b4
Johnny Benton <studiobenton@gmail.com>
2024-06-04 11:33:31 -0500

    merging asparagus function to main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,17 @@
+import { createPlan  }from "./plan.js"
+import { createAsparagus } from "./seeds/asparagus.js"
+
+createPlan()
+
+const asparagus = createAsparagus("asparagus", 24, 4)
+const yearlyPlan= createPlan()
+
+
 console.log("Welcome to the main module")
 
+console.log(asparagus)
+
+
+//build a module for each plant type (corn, soybeen, sunflower, asparagus, what, potato)
+// each module shouild have a function that has the following properties: i type ii. height, iii. output
+// one exception is corn. It should return an array with two identical objects in it with the proper keys and values
