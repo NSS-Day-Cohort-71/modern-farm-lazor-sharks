bbf0ffef524b02cd30a0f2e3801b2e3a57351e0b
James Campbell <jamesrcampbell.obb@gmail.com>
2024-06-04 14:52:59 -0500

    created the field module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,8 +6,9 @@ import { createSoybean } from "./seeds/soybean.js"
 import { createSunflower } from "./seeds/sunflower.js"
 import { createAsparagus } from "./seeds/asparagus.js"
 import { createCorn } from './seeds/corn.js'
+import { addPlant, usePlants } from './field.js'
 
-const yearlyPlan = createPlan()
+export const yearlyPlan = createPlan()
 
 const wheat = createWheat('Wheat', 230, 6)
 const potato = createPotato('Potato', 32, 2)
@@ -16,7 +17,11 @@ const soybean = createSoybean("soybean", 20, 4)
 const asparagus = createAsparagus("asparagus", 24, 4)
 const corn = createCorn("corn", 180, 6)
 
-console.log(corn)
+
+
+addPlant(yearlyPlan)
+
+console.log(usePlants())
 // build a module for each plant type (corn, soybean, sunflower, asparagus, wheat, potato)
 // each module should have a function that has the following properties: i. type, ii. height, iii. output
 // one exception is corn. it should return an array with two identical objects in it with the proper keys and values
